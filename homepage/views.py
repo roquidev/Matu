b98b98444d71094e7940977cd3c88e42b503356b
roy.quillca.contractor <roy.quillca.contractor@bbva.com>
2023-12-29 22:50:29 -0500

    add: homepage files added

--- homepage/views.py
+++ homepage/views.py
@@ -1,3 +1,113 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login, logout, authenticate
+from django.db import IntegrityError
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
+def home(request):
+    return render(
+        request=request,
+        template_name='homepage/home.html',
+        context={
+            # "form": UserCreationForm
+        }
+    )
+
+
+# @login_required
+def demo(request):
+    return render(
+        request=request,
+        template_name="demo.html",
+        context={}
+    )
+
+
+def signup(request):
+    if request.method == "GET":
+        return render(
+            request=request,
+            template_name='signup.html',
+            context={
+                "form": UserCreationForm
+            }
+        )
+    else:
+        if request.POST["password1"] == request.POST["password2"]:
+            try:
+                # Register user
+                user = User.objects.create_user(
+                    username=request.POST["username"],
+                    password=request.POST["password1"]
+                )
+                user.save()
+                # Creating cookie
+                login(
+                    request=request,
+                    user=user,
+                    backend='django.contrib.auth.backends.ModelBackend'
+                )
+                return redirect("demo")
+            except IntegrityError:
+                return render(
+                    request=request,
+                    template_name="signup.html",
+                    context={
+                        "form": UserCreationForm,
+                        "error": "Username already exists"
+                    }
+                )
+        return render(
+            request=request,
+            template_name="signup.html",
+            context={
+                "form": UserCreationForm,
+                "error": "Password do not match"
+            }
+        )
+
+
+# @login_required
+def signout(request):
+    logout(request=request)
+    return redirect(to="home")
+
+
+def signin(request):
+    if request.method == "GET":
+        return render(
+            request=request,
+            template_name='signin.html',
+            context={
+                "form": AuthenticationForm
+            }
+        )
+    else:
+        user = authenticate(
+            request=request,
+            username=request.POST["username"],
+            password=request.POST["password"]
+        )
+        if user is None:
+            return render(
+                request=request,
+                template_name='signin.html',
+                context={
+                    "form": AuthenticationForm,
+                    "error": "Username or passsword is incorrect"
+                }
+            )
+        else:
+            login(
+                request=request,
+                user=user,
+            )
+            # login(
+            #         request=request,
+            #         user=user,
+            #         backend='django.contrib.auth.backends.ModelBackend'
+            #     )
+            return redirect(to="demo")
