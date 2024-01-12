a07ab7da1a5abddc6308cc4c4a1b1d324d58f82e
royquillca <roquidata@gmail.com>
2024-01-12 11:33:32 -0500

    update: Models have been refactored

--- homepage/admin.py
+++ homepage/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import UserDetails, TravelPackage, Agency
 
 
 # Register your models here.
-# admin.site.register([UserDetails, TravelPackage, Agency])
+# admin.site.register([])
